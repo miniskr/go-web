6afec07269d628f18061f9b2590b900781b3455e
liuyt <liuyt@hc-vision.cn>
2020-11-02 14:16:26 +0800

    handleer

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import "net/http"
 
-func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("Hello world"))
-	})
+type myHandleer struct {}
 
-	http.ListenAndServe("localhost:8888", nil)
+func (m *myHandleer) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello world"))
+}
+
+func main() {
+
+	mh := myHandleer{}
+
+	server := http.Server{
+		Addr: "localhost:8888",
+		Handler: &mh,	
+	}
+
+	server.ListenAndServe()
 }
